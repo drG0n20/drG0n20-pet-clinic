0e586305ae68696ddefd8a96fdb4ff42fe331ae9
Br0n50n <marcinster@gmail.com>
2019-06-08 11:37:39 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/drg0n20/srping/drg0n20petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package drg0n20.srping.drg0n20petclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
