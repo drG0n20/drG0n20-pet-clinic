898ab3082761cb9f8d738d91560d4479b0116cbb
drG0n20 <marcinster@gmail.com>
2019-12-09 18:37:55 +0100

    added PetTypeService, closes #29

---
+++ pet-clinic-data/src/main/java/drg0n20petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package drg0n20petclinic.services;
+
+import drg0n20petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/drg0n20petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package drg0n20petclinic.services.map;
+
+import drg0n20petclinic.model.PetType;
+import drg0n20petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
