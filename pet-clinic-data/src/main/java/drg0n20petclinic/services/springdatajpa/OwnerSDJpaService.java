61fd8019dbe3ab0503d11e69e4d6227b73289b99
drG0n20 <marcinster@gmail.com>
2019-12-11 17:36:23 +0100

    add spring data jpa service for owners, closes #40

---
+++ pet-clinic-data/src/main/java/drg0n20petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,61 @@
+package drg0n20petclinic.services.springdatajpa;
+
+import drg0n20petclinic.model.Owner;
+import drg0n20petclinic.repositories.OwnerRepository;
+import drg0n20petclinic.repositories.PetRepository;
+import drg0n20petclinic.repositories.PetTypeRepository;
+import drg0n20petclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                             PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return null;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
