608cdff3ea7270bdc7f1bc523ce66e68f835c6f3
drG0n20 <marcinster@gmail.com>
2020-03-10 17:42:27 +0100

    fixed parse issue on PetType. Closes #66

--- pet-clinic-data/src/main/java/drg0n20petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/drg0n20petclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package drg0n20petclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -37,6 +38,7 @@ public class Pet extends BaseEntity {
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
